a04545e1b0d15083f2c231074b0f89c860a37379
madamdata <forks.andhope@gmail.com>
2021-09-25 15:20:03 +0800

    Now marks messages with downloaded attachments as read and sets their subdir to 'cur'. Uses the update() method to sync with the remote server using the correct UID. Note: using add() and discard() generated messages with the same content but different UID, which resulted in them being uploaded then re-downloaded. update() preserves the original UID so the server knows what to do.

--- attachscript.py
+++ attachscript.py
@@ -12,17 +12,27 @@ numMessages = len(a.keys())
 for key in a.keys():
     msg = a.get(key)
     sender = msg['From']
-    for x in msg.walk():
-        if x.get_content_disposition() == 'attachment':
-            filename = sender + x.get_filename()
-            filename = filename.replace(" ", "")
-            filename = filename.replace("<", "-")
-            filename = filename.replace(">", "-")
-            print("ATTACHMENT FOUND!!")
-            print(x.get_content_type())
-            print(filename)
-            with open('/home/pi/mailtest/attachTest/'+filename, 'wb') as fp:
-                fp.write(x.get_payload(decode=True))
+    if msg.get_subdir() == 'new':
+        #print(msg.get_subdir())
+        # print(msg.get_flags())
+        msg.set_subdir('cur')
+        msg.add_flag('S')
+        for x in msg.walk():
+            if x.get_content_disposition() == 'attachment':
+                filename = sender + x.get_filename()
+                filename = filename.replace(" ", "")
+                filename = filename.replace("<", "-")
+                filename = filename.replace(">", "-")
+                print("attachment found: " + x.get_content_type() + ": " + filename)
+                filepath = '/home/pi/mailtest/attachTest/' + filename
+                if os.path.exists(filepath): #check for duplicates
+                    print(filepath + " : File exists already! Not writing a new one.")
+                else:
+                    with open(filepath, 'wb') as fp:
+                        fp.write(x.get_payload(decode=True)) #unpack the payload into an actual file
+        a.update({key:msg}) #add the --modified-- message, with new flags and subdir, to the mailbox. Remember that 'msg' has no necessary relation to the actual file in the mailbox - it's just a representation that we manipulate. 
+
+
             
 
 #testing vim scp 1 2 3 4 5 commit
