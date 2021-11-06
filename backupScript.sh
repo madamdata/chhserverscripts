80ff7763fcec3056ded4ea46cc657abb83dcd90f
madamdata <forks.andhope@gmail.com>
2021-11-06 11:46:45 +0800

    now backs up attachscript.log

--- backupScript.sh
+++ backupScript.sh
@@ -3,8 +3,9 @@
 DATESTRING=$(date +%d\_%b\_%H:%M)
 WORKINGDIRECTORY="/home/pi/mail"
 BACKUPFILENAME="backup/$DATESTRING.zip"
+BACKUPLOGFILENAME="attachments/log/backup/aslog$DATESTRING.log"
 
-cd $WORKINGDIRECTORY && zip -r $BACKUPFILENAME attachments
+cd $WORKINGDIRECTORY && zip -r $BACKUPFILENAME attachments && mv attachments/log/attachscript.log $BACKUPLOGFILENAME
 echo "Backing up to $BACKUPFILENAME ..."
 #cp /home/pi/Polytope/server/evennia.db3 $BACKUPFILENAME
 
@@ -12,4 +13,5 @@ rclone sync $HOME/mail/backup iningDropbox:backup
 
 echo "Deleting files older than 7 days ..."
 find $WORKINGDIRECTORY/backup/* -mtime +7 -exec rm {} \;
+find $WORKINGDIRECTORY/attachments/log/backup/* -mtime +7 -exec rm {} \;
 
