32a76d9353272cd36f69e40788a277b74dedf7e3
madamdata <forks.andhope@gmail.com>
2021-11-15 20:49:00 +0800

    fixed bash script bugs to do with non escaped parentheses

--- scraperscript.sh
+++ scraperscript.sh
@@ -3,19 +3,19 @@
 cd $1
 
 #print any existing unprocessed pdfs - they can't be scraped.
-for f in ++*.pdf; do lp -d CHH_XEROX -oColorModel=Gray $f; done
+#for f in ++*.pdf; do lp -d CHH_XEROX -oColorModel=Gray $f; done
 
 #rename existing pdfs. No unprocessed pdfs left.
 for f in ++*.pdf; do mv $f ${f:2}; done
 
 #convert individual sheets in the excel files to csv
-for f in ++*.xlsx; do in2csv -n "$f" | xargs -I % sh -c "in2csv $f --sheet % > $f%.csv"; done
+for f in ++*.xlsx; do in2csv -n "$f" | xargs -I % bash -c "in2csv '$f' --sheet % > '$f'%.csv"; done
 
 #convert csvs to pdf for printing
 for f in ++*.csv; do libreoffice --headless --convert-to pdf $f; done
 
 #print pdfs
-for f in ++*.pdf; do lp -d CHH_XEROX -oColorModel=Gray $f; done 
+#for f in ++*.pdf; do lp -d CHH_XEROX -oColorModel=Gray $f; done 
 
 
 #scrape 
