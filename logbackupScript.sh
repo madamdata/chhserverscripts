861e681edde7032fd339a63b31d27182cafa8fc5
madamdata <forks.andhope@gmail.com>
2022-01-19 15:09:19 +0800

    overlapping logs

--- logbackupScript.sh
+++ logbackupScript.sh
@@ -5,8 +5,11 @@ WORKINGDIRECTORY="/home/pi/mail"
 BACKUPLOGFILENAME="attachments/log/backup/aslog_$DATESTRING.log"
 BACKUPSCRAPELOGFILENAME="attachments/log/backup/scrlog_$DATESTRING.log"
 
-cd $WORKINGDIRECTORY && mv attachments/log/attachscript.log $BACKUPLOGFILENAME && touch attachments/log/attachscript.log
-cd $WORKINGDIRECTORY && mv attachments/log/scrape.log $BACKUPSCRAPELOGFILENAME && touch attachments/log/scrape.log
+cd $WORKINGDIRECTORY && mv attachments/log/attachscript.log $BACKUPLOGFILENAME 
+cat $BACKUPLOGFILENAME | tail -n 25 > attachments/log/attachscript.log
+cd $WORKINGDIRECTORY && mv attachments/log/scrape.log $BACKUPSCRAPELOGFILENAME
+cat $BACKUPSCRAPELOGFILENAME | tail -n 25 > attachments/log/scrape.log
+
 echo "Backing up to $BACKUPLOGFILENAME ..."
 echo "Backing up to $BACKUPSCRAPELOGFILENAME ..."
 
