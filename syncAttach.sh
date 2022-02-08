593f50d8cbf0668860cecf92e42af29f92a69930
chhLinodeServer <po+aa@chh.sg>
2022-02-08 18:56:13 +0800

    .

--- syncAttach.sh
+++ syncAttach.sh
@@ -1,7 +1,7 @@
-#/bin/bash
+#!/bin/bash
 
 mbsync -a
 /usr/bin/python3 $HOME/mail/chhserverscripts/attachscript.py >> $HOME/mail/attachments/log/attachscript.log 2>&1 
-# rclone sync $HOME/mail/attachments iningDropbox:attachments >> $HOME/mail/attachments/log/sync.log 2>&1
+rclone sync $HOME/mail/attachments iningDropbox:attachments >> $HOME/mail/attachments/log/sync.log 2>&1
 
 $HOME/mail/chhserverscripts/scraperscript.sh $HOME/mail/attachments/rosenberg
